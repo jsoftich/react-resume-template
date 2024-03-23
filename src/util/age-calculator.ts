58b5480d03968e7a48ab23688522f7fc933c2c9a
John Softich <jsoftich@gmail.com>
2024-03-23 13:57:58 -0600

    Fixed function to be correct on number of days for all months, not just April

--- src/util/age-calculator.ts
+++ src/util/age-calculator.ts
@@ -3,13 +3,16 @@ export function CalculateDiff(myDate: number[]) {
   const monthNow = new Date().getMonth() + 1;
   const dayNow = +new Date().getDate();
 
+  const daysInMonth = (x: number, y: number): number => { return new Date(x, y, 0).getDate();}
+  let CurrentDaysInMonth = daysInMonth(yearNow,monthNow);
+
   let yearsDiff = yearNow - myDate[0];
   let monthsDiff = monthNow - myDate[1];
   let daysDiff = dayNow - myDate[2];
 
   if (daysDiff < 0) {
     monthsDiff -= 1;
-    daysDiff = monthNow !== 4 ? 30 - Math.abs(daysDiff) : 31 - Math.abs(daysDiff);
+    daysDiff = CurrentDaysInMonth - Math.abs(daysDiff);
   }
   if (monthsDiff < 0) {
     yearsDiff -= 1;
